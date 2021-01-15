352cb7d67510610c125c2bd317d7844b30b29ee1
shreyasinamdar007 <shreyasinamdar7@gmail.com>
2021-01-15 13:47:37 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { React, useState, useEffect } from "react";
+import "./App.css";
+import Navigate from "./components/Navbar/Navbar";
+import { Card } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigate />
+    </div>
+  );
+}
+
+export default App;
